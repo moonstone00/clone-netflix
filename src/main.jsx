5abb57e351cbbfaeb9a44c630e08d8c9e97f7390
Ghifani <ghifani03@gmail.com>
2025-08-08 22:31:38 +0700

    setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './pages/Landing/index.jsx'
+import { createBrowserRouter, RouterProvider  } from 'react-router-dom'
+
+import Landing from './pages/Landing/index.jsx'
+import Browse from './pages/Browse/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />
+  },
+  {
+    path: '/browse',
+    element: <Browse />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Landing() {
-    return (
-        <div>Landing</div>
-    )
+  return (
+    <div>Landing</div>
+  )
 }
