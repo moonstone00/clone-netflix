3c388d0f649d1fe5c51293ebe338e12396d3ce8c
Ghifani <ghifani03@gmail.com>
2025-08-09 13:53:34 +0700

    constans list footer

---
+++ src/constants/listFooter.js
@@ -0,0 +1,30 @@
+const LIST_FOOTER_EN = [
+    {
+        title: "FAQ",
+        link: "/"
+    },
+    {
+        title: "Help Center",
+        link: "/"
+    },
+    {
+        title: "Accounts",
+        link: "/"
+    },
+    {
+        title: "Investor Relations",
+        link: "/"
+    },
+    {
+        title: "Media Center",
+        link: "/"
+    },
+    {
+        title: "Jobs",
+        link: "/"
+    },
+    {
+        title: "Redeem Gift Cards",
+        link: "/"
+    },
+]
