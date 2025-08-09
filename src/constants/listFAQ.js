d340a76619e1adebe22003e6100c6be0d3625326
Ghifani <ghifani03@gmail.com>
2025-08-09 13:36:17 +0700

    constant list faq

---
+++ src/constants/listFAQ.js
@@ -0,0 +1,76 @@
+const FAQ_TITLE_ID = "Tanya Jawab Umum"
+const FAQ_TITLE_EN = "Frequently Asked Questions"
+
+const LIST_FAQ_ID = [
+    {
+        title: "Apa Itu Netflix",
+        desc: "Netflix adalah layanan streaming yang menawarkan berbagai acara TV pemenang penghargaan, film, anime, dokumenter, dan banyak lagi di ribuan perangkat yang terhubung ke Internet. Kamu bisa menonton sepuasnya, kapan pun kamu mau, tanpa satu iklan pun – semuanya dengan satu harga bulanan yang murah. Selalu ada tontonan baru dan acara TV serta film baru yang ditambahkan setiap minggu!",
+        isOpen: false
+    },
+    {
+        title: "Berapa biaya berlangganan Netflix?",
+        desc: "Tonton Netflix di smartphone, tablet, Smart TV, laptop, atau perangkat streaming-mu, semuanya dengan satu harga bulanan tetap Rentang harga mulai dari Rp54.000 hingga Rp186.000 per bulan. Tanpa biaya ekstra, tanpa kontrak.",
+        isOpen: false
+    },
+    {
+        title: "Di mana saya bisa menonton?",
+        desc: "Tonton di mana pun, kapan pun. Masuk ke akun Netflix-mu untuk menonton langsung di netflix.com dari komputer pribadi atau di perangkat yang terhubung ke Internet dan mendukung aplikasi Netflix, termasuk smart TV, smartphone, tablet, pemutar media streaming, dan konsol game. Kamu juga bisa men-download acara favoritmu dengan aplikasi iOS atau Android. Gunakan download untuk menonton saat kamu di perjalanan dan tidak punya koneksi Internet. Bawa Netflix ke mana saja.",
+        isOpen: false
+    },
+    {
+        title: "Bagaimana cara membatalkannya?",
+        desc: "Netflix fleksibel. Tanpa kontrak menyebalkan dan tanpa komitmen. Kamu dapat dengan mudah membatalkan akunmu secara online dengan dua kali klik. Tanpa biaya pembatalan – mulai atau hentikan akunmu kapan pun.",
+        isOpen: false
+    },
+    {
+        title: "Apa yang bisa ditonton di Netflix?",
+        desc: "Netflix memiliki pustaka lengkap yang berisi film panjang, film dokumenter, acara TV, anime, Netflix original pemenang penghargaan, dan lebih banyak lagi. Tonton sepuasnya, kapan pun kamu mau.",
+        isOpen: false
+    },
+    {
+        title: "Apakah Netflix sesuai bagi anak-anak?",
+        desc: "Pengalaman Netflix Anak disertakan dalam keanggotaan agar kamu punya kontrol orang tua saat anak-anak menikmati acara TV dan film untuk keluarga di rumah. Profil anak dilengkapi kontrol orang tua yang dilindungi PIN sehingga kamu bisa membatasi rating usia konten yang bisa ditonton anak dan memblokir judul tertentu yang tidak boleh ditonton anak-anak.",
+        isOpen: false
+    },
+
+]
+
+const LIST_FAQ_EN = [
+    {
+        title: "What is Netflix?",
+        desc: "Netflix is a streaming service that offers award-winning TV shows, movies, anime, documentaries, and more on thousands of connected devices. You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to watch, and new TV shows and movies are added every week!",
+        isOpen: false
+    },
+    {
+        title: "How much does Netflix cost?",
+        desc: "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one low monthly price. Plans range from Rp54,000 to Rp186,000 per month. No extra fees, no contracts.",
+        isOpen: false
+    },
+    {
+        title: "Where can I watch?",
+        desc: "Watch anywhere, anytime. Sign in to your Netflix account to watch instantly from netflix.com on your personal computer or on any internet-connected device that supports the Netflix app, including smart TVs, smartphones, tablets, streaming media players, and game consoles. You can also download your favorite shows with the iOS or Android app. Use downloads to watch while you're on the go and don't have an internet connection. Take Netflix with you.",
+        isOpen: false
+    },
+    {
+        title: "How do I cancel?",
+        desc: "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online with just two clicks. No cancellation fees – start or stop your account anytime.",
+        isOpen: false
+    },
+    {
+        title: "What can I watch on Netflix?",
+        desc: "Netflix has a complete library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, whenever you want.",
+        isOpen: false
+    },
+    {
+        title: "Is Netflix suitable for kids?",
+        desc: "The Netflix Kids experience is included with your membership so you have parental controls while kids enjoy family-friendly TV shows and movies at home. Kids profiles come with PIN-protected parental controls so you can restrict the age rating of content kids can watch and block specific titles you don’t want kids to see.",
+        isOpen: false
+    },
+]
+
+export { 
+    FAQ_TITLE_ID,
+    FAQ_TITLE_EN,
+    LIST_FAQ_ID,
+    LIST_FAQ_EN,
+}
