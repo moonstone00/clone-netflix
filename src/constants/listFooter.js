b5269d59c339448e7a1e01cafcb1fdf77d07443d
Ghifani <ghifani03@gmail.com>
2025-08-09 14:00:24 +0700

    update constants list footer

--- src/constants/listFooter.js
+++ src/constants/listFooter.js
@@ -1,30 +1,143 @@
 const LIST_FOOTER_EN = [
     {
         title: "FAQ",
-        link: "/"
+        url: "/",
     },
     {
         title: "Help Center",
-        link: "/"
+        url: "/",
     },
     {
-        title: "Accounts",
-        link: "/"
+        title: "Account",
+        url: "/",
     },
     {
         title: "Investor Relations",
-        link: "/"
+        url: "/",
     },
     {
         title: "Media Center",
-        link: "/"
+        url: "/",
     },
     {
         title: "Jobs",
-        link: "/"
+        url: "/",
     },
     {
         title: "Redeem Gift Cards",
-        link: "/"
+        url: "/",
     },
-]
+    {
+        title: "Buy Gift Cardsr",
+        url: "/",
+    },
+    {
+        title: "Ways to Watch",
+        url: "/",
+    },
+    {
+        title: "Terms of Use",
+        url: "/",
+    },
+    {
+        title: "Privacy",
+        url: "/",
+    },
+    {
+        title: "Cookie Preferences",
+        url: "/",
+    },
+    {
+        title: "Corporate Information",
+        url: "/",
+    },
+    {
+        title: "Contact Us",
+        url: "/",
+    },
+    {
+        title: "Speed Test",
+        url: "/",
+    },
+    {
+        title: "Legal Notices",
+        url: "/",
+    },
+    {
+        title: "Only on Netflix",
+        url: "/",
+    },
+];
+
+const LIST_FOOTER_ID = [
+    {
+        title: "Tanya Jawab",
+        url: "/",
+    },
+    {
+        title: "Pusat Bantuan",
+        url: "/",
+    },
+    {
+        title: "Akun",
+        url: "/",
+    },
+    {
+        title: "Hubungan Investor",
+        url: "/",
+    },
+    {
+        title: "Pusat Media",
+        url: "/",
+    },
+    {
+        title: "Lowongan Kerja",
+        url: "/",
+    },
+    {
+        title: "Tukar Kartu Hadiah",
+        url: "/",
+    },
+    {
+        title: "Beli Kartu Hadiah",
+        url: "/",
+    },
+    {
+        title: "Cara Menonton",
+        url: "/",
+    },
+    {
+        title: "Ketentuan Penggunaan",
+        url: "/",
+    },
+    {
+        title: "Privasi",
+        url: "/",
+    },
+    {
+        title: "Preferensi Cookie",
+        url: "/",
+    },
+    {
+        title: "Informamsi Perusahaan",
+        url: "/",
+    },
+    {
+        title: "Hubungi Kami",
+        url: "/",
+    },
+    {
+        title: "Uji Kecepatan",
+        url: "/",
+    },
+    {
+        title: "Informasi Legal",
+        url: "/",
+    },
+    {
+        title: "Hanya Di Netflix",
+        url: "/",
+    },
+];
+
+export { LIST_FOOTER_EN, LIST_FOOTER_ID }
