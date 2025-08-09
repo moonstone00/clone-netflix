a899a70a48fa67ea45579ed96ac5cd58a3e36827
Ghifani <ghifani03@gmail.com>
2025-08-09 13:22:51 +0700

    constant list content

---
+++ src/constants/listContent.js
@@ -0,0 +1,57 @@
+const LIST_CONTENT_1_ID = [
+    {
+        title: "Nikmati di TV-mu",
+        desc: "Tonton acara Smart TV, Playstation, Xbox, Chromecast, Apple TV, pemutar Blu-ray, dan banyak lagi."
+    }
+]
+
+const LIST_CONTENT_2_ID = [
+    {
+        title: "Download acara TV untuk menontonnya secara offline",
+        desc: "Simpan favoritmu dengan mudah agar selalu ada acara TV dan film yang bisa ditonton."
+    }
+]
+
+const LIST_CONTENT_3_ID = [
+    {
+        title: "Tonton di mana pun",
+        desc: "Streaming file dan acara TV tak terbatas di ponsel, tablet, laptop, dan TV-mu."
+    }
+]
+
+const LIST_CONTENT_4_ID = [
+    {
+        title: "Buat profile untuk anak",
+        desc: "Kirim anak-anak untuk berpetualang bersama karakter favorit mereka di dunia yang dibuat khusus untuk mereka-gratis dengan keanggotaanmu."
+    }
+]
+
+const LIST_CONTENT_1_EN = [
+    {
+        title: "Enjoy on your TV",
+        desc: "Watch on Smart TV, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+    }
+]
+
+const LIST_CONTENT_2_EN = [
+    {
+        title: "Download TV shows to watch offline",
+        desc: "Easily save your favorites so you always have something to watch."
+    }
+]
+
+const LIST_CONTENT_3_EN = [
+    {
+        title: "Watch anywhere",
+        desc: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
+    }
+]
+
+const LIST_CONTENT_4_EN = [
+    {
+        title: "Create profiles for kids",
+        desc: "Send kids on adventures with their favorite characters in a world made just for them—free with your membership."
+    }
+]
+
+export { LIST_CONTENT_1_ID, LIST_CONTENT_2_ID, LIST_CONTENT_3_ID, LIST_CONTENT_4_ID, LIST_CONTENT_1_EN, LIST_CONTENT_2_EN, LIST_CONTENT_3_EN, LIST_CONTENT_4_EN }
